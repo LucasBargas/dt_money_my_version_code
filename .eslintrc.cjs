28577f30bbcf8ce169fa0b3c9f15a5109c3a97c9
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-09 11:44:30 -0300

    Eslint and Prettier packages - installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,43 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+      'standard-with-typescript',
     ],
-  },
-}
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.json'],
+    },
+    plugins: ['react', '@typescript-eslint'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      '@typescript-eslint/semi': 'off',
+      semi: 'off',
+      '@typescript-eslint/comma-dangle': 'off',
+      'prettier/prettier': 'error',
+      'comma-dangle': 'off',
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  };
