47439a0c316f572fbe74a24846a789bd1c08893a
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-13 00:28:10 -0300

    Transaction modal button on Header - changed

--- src/templates/Header/styles.ts
+++ src/templates/Header/styles.ts
@@ -17,17 +17,16 @@ export const HeaderButtons = styled.div`
   gap: 1.25rem;
 
   button {
-    &:first-of-type {
-      background: ${({ theme }) => theme.colors.fieldActiveColor};
-      color: ${({ theme }) => theme.colors.fontColor};
-      font-weight: 500;
-      padding: .75rem;
-      border-radius: .325rem;
-      transition: .4s;
+  background: ${({ theme }) => theme.colors.fieldActiveColor};
+  color: ${({ theme }) => theme.colors.fontColor};
+  font-weight: 500;
+  padding: .75rem;
+  border-radius: .325rem;
+  font-size: 1rem;
+  transition: .4s;
 
-      &:hover {
-        filter: brightness(.9)
-      }
-    }
+  &:hover {
+    filter: brightness(.9)
+  }
   }
 `;
