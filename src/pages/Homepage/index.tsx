13170196242d26621752d4b8ae2a0b78f102958f
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-18 22:05:45 -0300

    Transaction Modal in development

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from '../../templates/Header';
+import { Modal } from '../../templates/Modal';
 
 export const Homepage = (): JSX.Element => {
   return (
     <React.Fragment>
       <Header />
+      <Modal />
     </React.Fragment>
   );
 };
