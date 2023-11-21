c2d73ed87cb18340a02db6207484cdc35893f8c4
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-21 11:00:20 -0300

    Transaction modal - developed

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from '../../templates/Header';
-import { TransactionModal } from '../../templates/TransactionModal';
+import { TransactionModal } from '../../components/TransactionModal';
 import { Summary } from '../../templates/Summary';
 import { TransactionsTable } from '../../templates/TransactionsTable';
 
